e68247ec6c60851db14f2ed5e4514843c04298cb
Zofia-Szybalska <74619487+Zofia-Szybalska@users.noreply.github.com>
2020-12-19 00:09:50 +0100

    Update CheckingDate.cs

--- Przelicznik_wieku/Przelicznik_wieku/CheckingDate.cs
+++ Przelicznik_wieku/Przelicznik_wieku/CheckingDate.cs
@@ -51,7 +51,9 @@ namespace Przelicznik_wieku
             if (year > current.Year || 
                (year == current.Year && month > current.Month) || 
                (year == current.Year && month == current.Month && day > current.Day) ||
-               (year == current.Year && month == current.Month && day == current.Day && hour > current.Hour))
+               (year == current.Year && month == current.Month && day == current.Day && hourextended[0] > current.Hour) ||
+               (year == current.Year && month == current.Month && day == current.Day && hourextended[0] == current.Hour && hourextended.Length == 2 && hourextended[1] > current.Second) ||
+               (year == current.Year && month == current.Month && day == current.Day && hourextended[0] == current.Hour && hourextended.Length == 3 && hourextended[2] == current.Second && hourextended[2] > current.Second))
             {
                 return "You couldn't have been born in the future, could you?";
                 
